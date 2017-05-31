809044f27e3952f0028b4beaa0e101b75a34ddf7
vhinesh ravi <vr8697@rit.edu>
2017-05-30 23:58:20 -0400

    Plain UI window without individual components

--- longest-palindromic-number/src/com/cirrusled/assessment/LongestPalindromicNumberUi.java
+++ longest-palindromic-number/src/com/cirrusled/assessment/LongestPalindromicNumberUi.java
@@ -1,14 +1,17 @@
 package com.cirrusled.assessment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 public class LongestPalindromicNumberUi extends Application
 {
-	
+	Stage window;
 	
 	/**
 	 * The main function
@@ -28,7 +31,9 @@ public class LongestPalindromicNumberUi extends Application
 	 */
 	public GridPane setUpGrid(GridPane grid)
 	{
-		
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(15);
+		grid.setHgap(10);
 		return grid;
 	}
 	
@@ -51,7 +56,22 @@ public class LongestPalindromicNumberUi extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
+		window = primaryStage; //Stage variable
+		window.setTitle("Largest-Palindromic-Number-Finder"); //setting title of UI
 		
+		List<Node> nodes = new ArrayList<Node>(); //list to hold UI components
+		
+		GridPane grid = new GridPane(); //grid layout
+		
+		grid = setUpGrid(grid);
+		
+		nodes = addElementsToGrid(nodes, grid);
+		
+		grid.getChildren().addAll(nodes); //adding the UI components to the grid layout.
+		
+		Scene scene = new Scene(grid, 350, 200); //setting dimensions of UI window
+		window.setScene(scene);//hosting scene on stage 
+		window.show();// displaying the UI.
 		
 	}
 
