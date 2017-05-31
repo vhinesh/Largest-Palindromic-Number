13d0b4be5038b253dba3f1668d2536cc137da2cc
vhinesh ravi <vr8697@rit.edu>
2017-05-31 18:55:12 -0400

    File Names Corrected

--- longest-palindromic-number/src/com/cirrusled/assessment/ConfirmBox.java
+++ longest-palindromic-number/src/com/cirrusled/assessment/ConfirmBox.java
@@ -8,50 +8,56 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
 /**
  * The program implements a confirm box window
+ * 
  * @author vhineshravi
  *
  */
-public class ConfirmBox 
-{
+public class ConfirmBox {
 	static boolean answer;
+
 	/**
 	 * The function creates a confirm dialog box.
-	 * @param title title of the box
-	 * @param message message to be shown
+	 * 
+	 * @param title
+	 *            title of the box
+	 * @param message
+	 *            message to be shown
 	 * @return user response
 	 */
-	public static boolean display(String title, String message)
-	{
-		Stage window = new Stage(); //Stage Variable
-		window.initModality(Modality.APPLICATION_MODAL); //Making the user deal with current window
-		window.setTitle(title); //Setting title of window
+	public static boolean display(String title, String message) {
+		Stage window = new Stage(); // Stage Variable
+		window.initModality(Modality.APPLICATION_MODAL); // Making the user deal
+															// with current
+															// window
+		window.setTitle(title); // Setting title of window
 		window.setWidth(250);
 		window.setHeight(250);
 		Label label = new Label();
 		label.setText(message);
-		
-		Button yesButton =  new Button("Yes");
-		Button noButton =  new Button("No");
-		//Capturing user input
-		yesButton.setOnAction(clickEvent ->{
+
+		Button yesButton = new Button("Yes");
+		Button noButton = new Button("No");
+		// Capturing user input
+		yesButton.setOnAction(clickEvent -> {
 			answer = true;
 			window.close();
 		});
-		
-		noButton.setOnAction(clickEvent ->{
+
+		noButton.setOnAction(clickEvent -> {
 			answer = false;
 			window.close();
 		});
-		
+
 		VBox layout = new VBox(10);
-		layout.getChildren().addAll(label,yesButton,noButton);
+		layout.getChildren().addAll(label, yesButton, noButton);
 		layout.setAlignment(Pos.CENTER);
 		Scene scene = new Scene(layout);
 		window.setScene(scene);
 		window.showAndWait();
-		
+
 		return answer;
 	}
 }
---
+++ longest-palindromic-number/src/com/cirrusled/assessment/LargestPalindromeSearch.java
@@ -0,0 +1,39 @@
+package com.cirrusled.assessment;
+import javafx.scene.control.TextField;
+
+public class LongestPalindromeSearch 
+{
+
+	long minValue, maxValue;
+	
+
+	public LongestPalindromeSearch(long minValue, long maxValue )
+	{
+		this.minValue=minValue;
+		this.maxValue=maxValue;
+	}
+	
+	public String searchPalindrome()
+	{
+		for(long temp = maxValue; temp >= minValue; temp--)
+		{
+			if(isPalindromic(temp))
+			{
+				return (temp+"");
+			}
+		}
+		return "No Plaindromic Numbers input range";
+	}
+	public boolean isPalindromic(long input)
+	{
+		String number = Long.toString(input);
+		StringBuffer transNumber = new StringBuffer(number);
+		transNumber = transNumber.reverse();
+		if(transNumber.toString().equals(number))
+		{
+			return true;
+		}
+		return false; 
+	}
+	
+}
