9602d754eee9bf07a93b040f152e2efeab2c96e9
vhinesh ravi <vr8697@rit.edu>
2017-05-30 23:20:25 -0400

    UI code structure

--- longest-palindromic-number/src/com/cirrusled/assessment/LongestPalindromicNumberUi.java
+++ longest-palindromic-number/src/com/cirrusled/assessment/LongestPalindromicNumberUi.java
@@ -1,22 +1,59 @@
 package com.cirrusled.assessment;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
+import java.util.List;
 
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 public class LongestPalindromicNumberUi extends Application
 {
-
+	
+	
+	/**
+	 * The main function
+	 * @param args
+	 */
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
-
-	@Override
-	public void start(Stage arg0) throws Exception 
+	
+	/**
+	 * Function to setup the grid layout's properties
+	 * 
+	 * @param grid the grid object argument
+	 * @return grid object
+	 */
+	public GridPane setUpGrid(GridPane grid)
 	{
-		// TODO Auto-generated method stub
+		
+		return grid;
+	}
+	
+	/**
+	 * The function adds individual elements to grid layout.
+	 * 
+	 * @param nodes The List to store the individual objects.
+	 * @param grid The grid object 
+	 * @return The List with UI components
+	 */
+	public List<Node> addElementsToGrid(List<Node> nodes, GridPane grid)
+	{
+	
+		return nodes;
+	}
+	
+	/**
+	 * The controller function
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception 
+	{
+		
 		
 	}
+
 
 }
